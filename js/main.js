d06742c9e7aa022ad3e74b0e82a6b8ae890cfb3c
Cole <colemag383@gmail.com>
2019-08-01 19:18:08 +1200

    Sort github repos by date (default was alphabetical)

--- js/main.js
+++ js/main.js
@@ -15,13 +15,13 @@ function toggleNoHeight() {
 }
 
 function meOpenClose() {
- 
+
 
   document.querySelector("a.me").addEventListener('click', toggleNoHeight);
   document.querySelector(".pop-up>h2").addEventListener('click', toggleNoHeight);
 }
 
-function createRepoEl(repo){
+function createRepoEl(repo) {
   let div = document.createElement("div");
   div.innerHTML = `<h3><a href="${repo.html_url}">${repo.name}</a></h3>
   <span><a href="${repo.html_url}">${repo.description}</a></span>`;
@@ -33,8 +33,13 @@ async function recentRepos() {
   const response = await fetch('https://api.github.com/users/cole-maguire/repos');
   const repos = await response.json();
 
-  //We only want the first four repors to added, for space reasons
-  Array.from(repos).slice(0,3).forEach(repo => githubPanel.append(createRepoEl(repo)));
+  //We only want the first four most recently updateds, for space reasons
+  let reposByDate = Array.from(repos).sort((a, b) => {
+    let dateA = new Date(a.pushed_at);
+    let dateB = new Date(b.pushed_at);
+    return dateB - dateA;
+  });
+  reposByDate.slice(0, 3).forEach(repo => githubPanel.append(createRepoEl(repo)));
 
 }
 
